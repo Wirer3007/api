f52deae2491092f561aa2d1a253ceaded7b28bd8
Lab05_etec <Lab05_etec@etec>
2026-04-02 13:39:42 -0300

    gg

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import os
 app = Flask(__name__)
 
 usuarios = [
-    {"id": 1, "nome": "Wirer"}
+    {"id": 1, "nome": "Wirer"},
+    {"id": 2, "nome": "Dark"}
 ]
 
 
 @app.route("/usuarios", methods=["GET"])
 def listar_usuarios():
     return jsonify(usuarios)
+
 
 @app.route("/usuarios", methods=['POST'])
 def criar_usuario():
@@ -19,8 +21,26 @@ def criar_usuario():
     usuarios.append(novo)
     return jsonify(novo), 201
 
+@app.route('/usuarios/<int:id>', methods=['PUT'])
+def atualizar_usuario(id):
+    for usuario in usuarios:
+        if usuario['id'] == id:
+            dados = request.json
+            usuario['nome'] = dados.get('nome', usuario['nome'])
+            return jsonify(usuario)
+    return {"erro": "Usuário não encontrado"}, 404
+
+
+@app.route('/usuarios/<int:id>', methods=['DELETE'])
+def deletar_usuario(id):
+    for usuario in usuarios:
+        if usuario['id'] == id:
+            usuarios.remove(usuario)
+            return {"mensagem": "Usuário removido"}
+    return {"erro": "Usuário não encontrado"}, 404
+
 
 
 
 if __name__ == "__main__":
-    app.run (debug = true)
+    app.run (debug = True)
