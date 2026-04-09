a14f47bcab206a2672f010f04eb6ae711121e7e2
Lab05_etec <Lab05_etec@etec>
2026-04-09 13:35:41 -0300

    tentei

--- app.py
+++ app.py
@@ -4,22 +4,49 @@ import os
 app = Flask(__name__)
 
 usuarios = [
-    {"id": 1, "nome": "Wirer"},
-    {"id": 2, "nome": "Dark"}
+    {   "id": 1,
+        "titulo": "Dom Casmurro",
+        "autor": "Machado de Assis",
+        "ano": 1899
+        },
+
+    {   "id": 2,
+        "titulo": "Saboroso Cadaver",
+        "autor": "Darkside",
+        "ano": 2022
+        },
+
+    {   "id": 3,
+        "titulo": "Kaiju n8",
+        "autor": "Matsumoto",
+        "ano": 2019
+        },
 ]
+
+
 
 
 @app.route("/usuarios", methods=["GET"])
 def listar_usuarios():
     return jsonify(usuarios)
+    
 
 
 @app.route("/usuarios", methods=['POST'])
 def criar_usuario():
+ if not usuarios.get('titulo') or not usuarios.get('autor'):
+    if usuarios['ano'] < 0: return {"erro": "Ano inválido"}, 400
+    for l in usuarios:
+      if l['titulo'] == usuarios['titulo']:
+       return {"erro": "Livro já cadastrado"}, 400
     novo = request.json
     novo['id'] = len(usuarios) + 1
     usuarios.append(novo)
-    return jsonify(novo), 201
+    return {
+    "mensagem": "Livro cadastrado com sucesso",
+    "livro": novo
+    }, 201
+ return {"erro": "Título e autor são obrigatórios"}, 400
 
 @app.route('/usuarios/<int:id>', methods=['PUT'])
 def atualizar_usuario(id):
